c29014a84786426973a0d83179806d849edf4bd5
Snehadeep71 <147100218+Snehadeep71@users.noreply.github.com>
2024-06-04 19:20:04 +0530

    first commit made

---
+++ js/script.js
@@ -0,0 +1,43 @@
+let closer=document.querySelector('#closer');
+closer.onclick=()=>{
+    closer.style.display='none';
+    navbar.classList.remove('active');
+    cart.classList.remove('active');
+    loginForm.classList.remove('active');
+}
+let navbar=document.querySelector('.navbar');
+document.querySelector('#menu-btn').onclick=()=>{
+    closer.style.display='block';
+    navbar.classList.toggle('active');
+}
+let cart=document.querySelector('.shopping-cart');
+document.querySelector('#cart-btn').onclick=()=>{
+    closer.style.display='block';
+    cart.classList.toggle('active');
+}
+let loginForm=document.querySelector('.login-form');
+document.querySelector('#login-btn').onclick=()=>{
+    closer.style.display='block';
+    loginForm.classList.toggle('active');
+}
+let searchForm=document.querySelector('.header .search-form');
+document.querySelector('#search-btn').onclick=()=>{
+    
+    searchForm.classList.toggle('active');
+}
+
+window.onscroll=()=>{
+    searchForm.classList.remove('active');
+}
+let slide=document.querySelectorAll('.home .slide-container .slide');
+let index=0;
+function next(){
+    slide[index].classList.remove('active');
+    index=(index+1)% slide.length;
+    slide[index].classList.add('active');
+}
+function prev(){
+    slide[index].classList.remove('active');
+    index=(index-1 + slide.length)% slide.length;
+    slide[index].classList.add('active');
+}
